602bc0da813a0fb7a5e95c7a313a9580d1b7d608
Djafny <jafnyabdad@gmail.com>
2023-01-31 10:53:15 +0100

    v2

--- main.js
+++ main.js
@@ -11,13 +11,33 @@
 //     .then(response => {
 //         console.log(response);
 
-//         //Current Free Games
+//            //Last Free Games
 //         document.getElementById("card-1").innerHTML = response.freeGames.current[0].title;
+//         document.getElementById("card-1-img").src = response.freeGames.current[0].keyImages[0].url;
+//         document.getElementById("card-1-price").innerHTML = response.freeGames.current[0].price.totalPrices.fmtPrice.discountPrice;
+//         document.getElementById("card-1-original-price").innerHTML = response.freeGames.current[0].price.totalPrices.fmtPrice.originalPrice;
+
+
+//            //Current Free Games
 //         document.getElementById("card-2").innerHTML = response.freeGames.current[1].title;
+//         document.getElementById("card-2-img").src = response.freeGames.current[0].keyImages[0].url;
+//         document.getElementById("card-2-price").innerHTML = response.freeGames.current[0].price.totalPrices.fmtPrice.discountPrice;
+//         document.getElementById("card-2-original-price").innerHTML = response.freeGames.current[0].price.totalPrices.fmtPrice.originalPrice;
+
 //         document.getElementById("card-3").innerHTML = response.freeGames.current[2].title;
+//         document.getElementById("card-3-img").src = response.freeGames.current[0].keyImages[0].url;
+//         document.getElementById("card-3-price").innerHTML = response.freeGames.current[0].price.totalPrices.fmtPrice.discountPrice;
+//         document.getElementById("card-3-original-price").innerHTML = response.freeGames.current[0].price.totalPrices.fmtPrice.originalPrice;
 
 //         //Upcoming Games
 //         document.getElementById("card-4").innerHTML = response.freeGames.upcoming[0].title;
+//         document.getElementById("card-4-img").src = response.freeGames.current[0].keyImages[0].url;
+//         document.getElementById("card-4-price").innerHTML = response.freeGames.current[0].price.totalPrices.fmtPrice.discountPrice;
+//         document.getElementById("card-4-original-price").innerHTML = response.freeGames.current[0].price.totalPrices.fmtPrice.originalPrice;
+
 //         document.getElementById("card-5").innerHTML = response.freeGames.upcoming[1].title;
+//         document.getElementById("card-5-img").src = response.freeGames.current[0].keyImages[0].url;
+//         document.getElementById("card-5-price").innerHTML = response.freeGames.current[0].price.totalPrices.fmtPrice.discountPrice;
+//         document.getElementById("card-5  -original-price").innerHTML = response.freeGames.current[0].price.totalPrices.fmtPrice.originalPrice;
 //     })
 //     .catch(err => console.error(err));
